33cb23d1f6dd5178d69646dc3b8647ec6d192900
Rosembel Fermin <rfermin@tekhsoft.com>
2018-08-30 11:54:33 -0400

    Se construyo el Rest Full

--- Sales.Common/Models/Response.cs
+++ Sales.Common/Models/Response.cs
@@ -1,19 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sales.Common.Models
+﻿namespace Sales.Common.Models
 {
     public class Response
     {
         public bool IsSuccess { get; set; }
 
-        public String Message { get; set; }
+        public string Message { get; set; }
 
-        public Object Result { get; set; }
-
-
+        public object Result { get; set; }
     }
 }
--- Sales/Sales/Services/ApiServices.cs
+++ Sales/Sales/Services/ApiServices.cs
@@ -1,15 +1,52 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Sales.Services
+﻿namespace Sales.Services
 {
+    using System;
+    using System.Collections.Generic;
+    using System.Net.Http;
+    using System.Threading.Tasks;
+    using Common.Models;
+    using Newtonsoft.Json;
+
     public class ApiServices
     {
         public async Task<Response> GetList<T>(string urlBase, string prefix, string controller)
         {
+            try
+            {
+                var client = new HttpClient();
+                client.BaseAddress = new Uri(urlBase);
+                var url = $"{prefix}{controller}";
+                var response = await client.GetAsync(url);
+                var answer = await response.Content.ReadAsStringAsync();
+                if (!response.IsSuccessStatusCode)
+                {
+                    return new Response
+                    {
+                        IsSuccess = false,
+                        Message = answer,
 
+                    };
+                }
+
+                var list = JsonConvert.DeserializeObject<List<T>>(answer);
+                return new Response
+                {
+                    IsSuccess = true,
+                    Result = list,
+                };
+
+            }
+            catch (System.Exception ex)
+            {
+                return new Response
+                {
+                    IsSuccess = false,
+                    Message = ex.Message,
+
+                };
+
+                throw;
+            }
         }
     }
 }
